6d0030e9e47170b711e632df4d0a9e96b0bbbbb4
Adam Watkins <adamjwatkins@gmail.com>
2021-02-13 13:59:32 +0000

    Revert to using Dictionary and the List for the view

--- TourBooker.Logic/AppData.cs
+++ TourBooker.Logic/AppData.cs
@@ -9,13 +9,12 @@ namespace Pluralsight.AdvCShColls.TourBooker.Logic
 	public class AppData
 	{
 		public List<Country> AllCountries { get; private set; }
-        public SortedList<string,Country> AllCountriesByKey { get; set; }
+        public Dictionary<string,Country> AllCountriesByKey { get; set; }
         public void Initialize(string csvFilePath)
 		{
 			CsvReader reader = new CsvReader(csvFilePath);
 			this.AllCountries = reader.ReadAllCountries().OrderBy(x=>x.Name).ToList();
-			var dictionary = reader.ReadAllCountries().ToDictionary(x => x.Code, StringComparer.OrdinalIgnoreCase);
-			this.AllCountriesByKey = new SortedList<string, Country>(dictionary);
+			this.AllCountriesByKey = reader.ReadAllCountries().ToDictionary(x => x.Code, StringComparer.OrdinalIgnoreCase);
 		}
 	}
 }
