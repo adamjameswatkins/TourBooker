1dc1275251bff050dee0120cfc683b2197b39032
Adam Watkins <adamjwatkins@gmail.com>
2021-02-13 14:44:57 +0000

    Override Equals, ==, and != for CountryCode

--- TourBooker.Logic/CountryCode.cs
+++ TourBooker.Logic/CountryCode.cs
@@ -1,4 +1,6 @@
-﻿namespace Pluralsight.AdvCShColls.TourBooker.Logic
+﻿using System;
+
+namespace Pluralsight.AdvCShColls.TourBooker.Logic
 {
     public class CountryCode
     {
@@ -10,5 +12,31 @@
         }
 
         public override string ToString() => Value;
+
+        public override bool Equals(object obj)
+        {
+            if(obj is CountryCode other)
+            {
+                return StringComparer.OrdinalIgnoreCase.Equals(this.Value, other.Value);
+            }
+            return false;
+        }
+
+        public static bool operator ==(CountryCode lhs, CountryCode rhs)
+        {
+            if (lhs != null)
+            {
+                return lhs.Equals(rhs);
+            }
+            else
+            {
+                return rhs == null;
+            }
+        }
+
+        public static bool operator !=(CountryCode lhs, CountryCode rhs)
+        {
+            return !(lhs == rhs);
+        }
     }
 }
